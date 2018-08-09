7441f5a1eef401d516141a8e458689ad4a26b655
Devil7DK <dineshthangavel47@gmail.com>
2018-08-09 06:27:42 +0530

    libbootimg: add shared library target

--- Android.mk
+++ Android.mk
@@ -5,3 +5,10 @@ LOCAL_SRC_FILES				:= mkbootimg.c unpackbootimg.c
 LOCAL_STATIC_LIBRARIES			:= libmincrypt libcutils libc
 LOCAL_MODULE				:= libbootimg
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES				:= mkbootimg.c unpackbootimg.c
+LOCAL_STATIC_LIBRARIES			:= libmincrypt
+LOCAL_SHARED_LIBRARIES			:= libcutils libc
+LOCAL_MODULE				:= libbootimg
+include $(BUILD_SHARED_LIBRARY)
